e85b4ccd2ae5f35555c87470bc53bd1dd926dc80
t7169253 <t7169253@live.tees.ac.uk>
2018-03-29 16:15:43 +0100

    Code overhauled, Code now allows for + Added Mobile/tablet/mobile CSS for better breakpoints + Better Mobile first code

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,29 @@
  * Created by t7169253 on 27/03/2018.
  */
 
+/*
+ Description:
+ This website is for the ICA group website product
+
+ Group Members
+ Michael Watterston
+ Simon Dean
+ Liam
+ Miles
+
+ gitHub: https://github.com/miikeymike/WEB_PM_GROUPWORKYEAR1.git
+*/
 
 // Testing to see if Javascript file link works
 
 var jsTest = "Javascript Resource Working &#10004;";
 document.getElementById("testBox").innerHTML = jsTest;
 
+// Sticky Menu for Index.html
+
 window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("navbar");
+var navbar = document.getElementById("myTopnav");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
@@ -19,4 +33,14 @@ function myFunction() {
     } else {
         navbar.classList.remove("sticky");
     }
-}
+};
+
+// Make menu burger button
+function myFunction2() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+};
