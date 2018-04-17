ebb97d99df7824c09caee017796c866cab045472
Michael <34450892+miikeymike@users.noreply.github.com>
2018-04-17 06:50:11 +0100

    Finished Website

--- Base Template/scripts/main.js
+++ Base Template/scripts/main.js
@@ -1,10 +1,9 @@
-/**
- * Created by t7169253 on 27/03/2018.
- */
-
 /*
  Description:
  This website is for the ICA group website product
+ Individual 60%
+
+ Coded by: Michael Watterston T7169253
 
  Group Members
  Michael Watterston
@@ -19,6 +18,7 @@
 
 //var jsTest = "Javascript Resource Working &#10004;";
 //document.getElementById("testBox").innerHTML = jsTest;
+
 
 // Sticky Menu for Index.html
 
