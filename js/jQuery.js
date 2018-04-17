c7d0f1ff3b5b4cb02678cc4eadc109e7e48de2cd
EvilErnie44 <33355726+EvilErnie44@users.noreply.github.com>
2018-04-17 14:46:48 +0100

    Add files via upload

--- js/jQuery.js
+++ js/jQuery.js
@@ -9,6 +9,8 @@ $(document).ready(function(){
     });
 });
 
+
+//Michaels Function
 var yourNavigation = $(".menuItems");
     stickyDiv = "sticky";
     yourHeader = $('.navwrap').height();
@@ -19,6 +21,28 @@ $(window).scroll(function() {
   } else {
     yourNavigation.removeClass(stickyDiv);
   }
+});
+
+//Smoth Sroll function
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $(".smoothScr").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
 
 //The next 3 functions are checking the width of viewport on load as to ensure the layout is consistent even if page is not cached.
--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ function populateContainer() {
 function myFunction2() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-        x.className += " responsive";
+        x.className += "responsive";
     } else {
         x.className = "topnav";
     }
